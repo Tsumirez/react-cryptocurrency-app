1c1f17e0f42ed939adbbc027211ac6743320dc69
Tsumirez <tsumirez@zoho.com>
2022-04-12 21:21:58 +0200

    Cryptocurrencies component now fetches and displays cryptocurrency data from redux storage.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -20,20 +20,4 @@ export const cryptoApi = createApi({
 })
 
 export const { useGetCryptosQuery } = cryptoApi;
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       tiers: '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-
-//     }
-//   };
 
