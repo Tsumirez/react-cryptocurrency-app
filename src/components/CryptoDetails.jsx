1ddeede9bcfa7e3c2d6cef8412899ed7d8b4046a
Tsumirez <tsumirez@zoho.com>
2022-04-14 16:06:14 +0200

    LineChart component is now finished, providing chart for a chosen cryptocurrency's value over time in the past.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -32,8 +32,6 @@ const CryptoDetails = () => {
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
-  console.log(coinHistory);
-
   const stats = [
     {
       title: "Price to USD",
